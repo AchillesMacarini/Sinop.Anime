8ff344babccc53ddb677dcbcb33419d9e1e0b6ea
AchillesMacarini <capivaradoacre@gmail.com>
2019-08-06 11:35:10 -0300

    Merge branch 'master' of https://github.com/AchillesMacarini/Sinop.Anime

--- app/src/main/java/com/teknestige/sinop/AnimeActivity.java
+++ app/src/main/java/com/teknestige/sinop/AnimeActivity.java
@@ -1,58 +1,47 @@
 package com.teknestige.sinop;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.Gravity;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.teknestige.classes.CreateList;
-import com.teknestige.classes.MyAdapter;
 import com.teknestige.entidades.Anime;
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
-
 import com.teknestige.entidades.Usuario;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Date;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 
 import DbControler.BDHelper;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 public class AnimeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -121,6 +110,21 @@ public class AnimeActivity extends AppCompatActivity
         }
     }
 
+    public void buildAlertDialog(View v){
+        // custom dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+        dialog.setTitle("Title...");
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 
     public void arrayAnimes() throws IOException, JSONException {
 
